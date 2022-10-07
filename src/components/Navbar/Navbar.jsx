0a790f955db6ad7abe0f9e99c623fd6bfff82c17
fresm4at <fernandesvalentin1235@gmail.com>
2022-10-07 17:33:10 +0200

    add smallscreen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { MdOutlinerestaurantMenu}  from 'react-icons/md'
+import { MdOutlineRestaurantMenu}  from 'react-icons/md'
 
 import images from '../../constants/images'
 import './Navbar.css';
 
-const Navbar = () => (
-  <nav className='app__navbar'>
-    <div className='app__navbar-logo'>
-      <img src={images.laCabane} alt="app logo"/>
-    </div>
-    <ul className='app__navbar-links'>
-      <li className='app__opensans'><a href='#home'>Acceuil</a></li>
-      <li className='app__opensans'><a href='#menu'>Menu</a></li>
-      <li className='app__opensans'><a href='#contact'>Contact</a></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  return(
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <a>
+          <img src={images.laCabane} alt="app logo"/>
+        </a>
+      </div>
+      <ul className='app__navbar-links'>
+        <li className='app__opensans'><a href='#home'>Acceuil</a></li>
+        <li className='app__opensans'><a href='#menu'>Menu</a></li>
+        <li className='app__opensans'><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color='#fff' fontsize={27} onClick={()=> {setToggleMenu(true)}}/>
+
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu fontsize={27} className='overlay__close' onClick={()=>{setToggleMenu(false)}}/>
+          <ul className='app__navbar-smallscreen-links'>
+          <li className='app__opensans'><a href='#home'>Acceuil</a></li>
+          <li className='app__opensans'><a href='#menu'>Menu</a></li>
+          <li className='app__opensans'><a href='#contact'>Contact</a></li>
+        </ul>
+        </div>
+        )}
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
