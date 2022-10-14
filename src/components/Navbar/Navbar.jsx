05701261d2751bcf45e0f3b3ac4413d4a92bf5f9
fresm4at <fernandesvalentin1235@gmail.com>
2022-10-14 18:01:13 +0200

    clean navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,27 +10,26 @@ const Navbar = () => {
   return(
     <nav className='app__navbar'>
       <ul className='app__navbar-links'>
-        <li className='app__opensans'><a href='#home'>Acceuil</a></li>
-        <li className='app__opensans'><a href='#menu'>Menu</a></li>
+        <li className='p__opensans'><a href='#home'>Acceuil</a></li>
+        <li className='p__opensans'><a href='#menu'>Menu</a></li>
         <div className='app__navbar-logo'>
         <a href='#home'>
           <p className='p__opensans'>La cabane du Mil'lieu</p>
         </a>
         </div>
-        <li className='app__opensans'><a href='#photo'>Photos</a></li>
-        <li className='app__opensans'><a href='#contact'>Contact</a></li>
+        <li className='p__opensans'><a href='#photo'>Photos</a></li>
+        <li className='p__opensans'><a href='#contact'>Contact</a></li>
       </ul>
       <div className='app__navbar-smallscreen'>
         <GiHamburgerMenu color='#fff' fontsize={27} onClick={()=> {setToggleMenu(true)}}/>
-        
         {toggleMenu && (
           <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
             <MdOutlineRestaurantMenu fontsize={27} className='overlay__close' onClick={()=>{setToggleMenu(false)}}/>
-          <ul className='app__navbar-smallscreen-links'>
-          <li className='app__opensans'><a href='#home'>Acceuil</a></li>
-          <li className='app__opensans'><a href='#menu'>Menu</a></li>
-          <li className='app__opensans'><a href='#photo'>Photos</a></li>
-          <li className='app__opensans'><a href='#contact'>Contact</a></li>
+        <ul className='app__navbar-smallscreen-links'>
+          <li className='p__opensans'><a href='#home'>Acceuil</a></li>
+          <li className='p__opensans'><a href='#menu'>Menu</a></li>
+          <li className='p__opensans'><a href='#photo'>Photos</a></li>
+          <li className='p__opensans'><a href='#contact'>Contact</a></li>
         </ul>
         </div>
         )}
