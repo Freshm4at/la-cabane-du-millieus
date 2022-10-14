4de0db4473b46b046034f2096b6a42d92ce70b59
fresm4at <fernandesvalentin1235@gmail.com>
2022-10-14 17:47:32 +0200

    Update navbar, change logo and dispo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,25 +9,27 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
   return(
     <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
-        <a href='#home'>
-          <img src={images.laCabane} alt="app logo"/>
-        </a>
-      </div>
       <ul className='app__navbar-links'>
         <li className='app__opensans'><a href='#home'>Acceuil</a></li>
         <li className='app__opensans'><a href='#menu'>Menu</a></li>
+        <div className='app__navbar-logo'>
+        <a href='#home'>
+          <p className='p__opensans'>La cabane du Mil'lieu</p>
+        </a>
+        </div>
+        <li className='app__opensans'><a href='#photo'>Photos</a></li>
         <li className='app__opensans'><a href='#contact'>Contact</a></li>
       </ul>
       <div className='app__navbar-smallscreen'>
         <GiHamburgerMenu color='#fff' fontsize={27} onClick={()=> {setToggleMenu(true)}}/>
-
+        
         {toggleMenu && (
           <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
             <MdOutlineRestaurantMenu fontsize={27} className='overlay__close' onClick={()=>{setToggleMenu(false)}}/>
           <ul className='app__navbar-smallscreen-links'>
           <li className='app__opensans'><a href='#home'>Acceuil</a></li>
           <li className='app__opensans'><a href='#menu'>Menu</a></li>
+          <li className='app__opensans'><a href='#photo'>Photos</a></li>
           <li className='app__opensans'><a href='#contact'>Contact</a></li>
         </ul>
         </div>
