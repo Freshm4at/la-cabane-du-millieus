a3fe082e02c6f3c73a73bfc649dba10e57c2ab90
fresm4at <fernandesvalentin1235@gmail.com>
2022-10-17 11:12:46 +0200

    clean intro video code

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
-import {BsFillPlayFill, BsPauseFill} from 'react-icons/bs'
 import {meal} from '../../constants'
 import './Intro.css';
 
 const Intro = () => {
-  const [playVideo,setplayVideo] = useState(false);
   const vidRef = React.useRef();
-  const handleVideo = () =>{
-    setplayVideo((prevPlayvideo)=> !prevPlayvideo )
-    
-    if(playVideo){
-      vidRef.current.pause()
-    }else{
-      vidRef.current.play()
-    }
-  }
   return(
   <div className='app__video'>
     <video src={meal}
