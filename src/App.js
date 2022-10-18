6d4b8366511d5926a02295639000d2df1529db12
fresm4at <fernandesvalentin1235@gmail.com>
2022-10-18 16:49:50 +0200

    firebase integration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { AboutUs, FindUs, Footer, Gallery, Header, Intro } from './container';
 import { Navbar } from './components';
+import Firebase from 'firebase';
+import config from './firebaseConfig';
 import './App.css';
 
 const App = () => (
---
+++ src/firebaseConfig.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyCshrqhHugjyyDi9TCHkTFd2v16hTIBZwg",
+    authDomain: "js-website-restaurant.firebaseapp.com",
+    databaseURL: "https://js-website-restaurant-default-rtdb.europe-west1.firebasedatabase.app/"
+  };
+  
+  export default config;
