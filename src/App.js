09bd9bcaf97565ee6970136e98ca9a892b18bb66
fresm4at <fernandesvalentin1235@gmail.com>
2022-10-24 10:02:25 +0200

    remove firebase bug and add footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { AboutUs, FindUs, Footer, Gallery, Header, Intro } from './container';
 import { Navbar } from './components';
-import Firebase from 'firebase';
-import config from './firebaseConfig';
 import './App.css';
 
 const App = () => (
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
 import './Footer.css';
+import { FooterOverlay } from '../../components';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <div className='app__footer-links'>
+      <div className='app__footer-links_contact'>
+        <h1 className='app__footer-headtext'>Contact</h1>
+        <p className='p__opensans'>hello</p>
+        <p className='p__opensans'>hello</p>
+      </div>
+      <div className='app__footer-links_logo'>
+      <a href='#home'>
+          <p className='p__opensans'>La cabane du Mil'lieu</p>
+      </a>
+      </div>
+      <div className='app__footer-links_works'>
+        <h1 className='app__footer-headtext'>Horairre d'ouverture</h1>
+        <p className='p__opensans'>Lundi-Vendredi : </p>
+        <p className='p__opensans'>hello</p>
+        <p className='p__opensans'>Samedi-Dimanche : </p>
+        <p className='p__opensans'>hello</p>
+      </div>
+    </div>
+    <div className='app__footer-copyright'>
+        <p className='p__opensans'>2022 La Cabane du Mil'Lieus, Tout droits réservés.</p>
+      </div>
   </div>
 );
 
