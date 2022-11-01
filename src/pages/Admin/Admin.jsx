600855df9f5c04abf2475ed062122bb74bde2dde
Freshm4at <fernandesvalentin1235@gmail.com>
2022-11-01 22:48:04 +0100

    add txt changing

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -3,37 +3,45 @@ import axios from 'axios';
 import './Admin.css'
 import { images } from '../../constants';
 import { useState } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+
+
 
 const Admin = () => {
   const [image, setImage] = useState(null);
   const [img,setImg] = useState(null)
-  const HandleClick = () => {
+  const [lang, setLang] = useState(null);
+  const [txt,setTxt] = useState(null)
+
+  const handleChange = (event) => {
+    setLang(event.target.value);
+  };
+  const handleClickTxt = () =>{
+    axios.post(`http://localhost:4000/txt-upload-${lang}`, txt)
+    .then(res => {
+      console.log('Axios response: ', res)
+    })
+  }
+
+  const HandleClickImg = () => {
     axios.post('http://localhost:4000/image-upload', image)
     .then(res => {
       console.log('Axios response: ', res)
     })
-    .then(res => {
-      //WE CAN REMOVE THIS NEXT LINE
-      console.log('Axios POST request successful!') 
-      console.log('Axios response: ', res)
-    })
   }
   const getFileInfo = (e) => {
-    //NOTE THE ADDITION OF 'e' PARAMETER
-        console.log('File info working!')
-        console.log(e.target.files[0]);
         const formData = new FormData(); 
         //FILE INFO NAME WILL BE "my-image-file"
         formData.append('my-image-file', e.target.files[0], e.target.files[0].name);
         setImage(formData);
+        setTxt(formData)
         setImg(URL.createObjectURL(e.target.files[0]))
     
       }
-  const handleChange = () => { 
-
-    console.log('The checkbox was toggled'); 
-    
-  }; 
 
   return (
     <div className="app__admin">
@@ -43,26 +51,29 @@ const Admin = () => {
           <p className='p__opensans' style={{"font-size":'50px',"padding-bottom":'2rem'}}>Changer une image</p>
           <input type="file" onChange={getFileInfo}></input>
           <img src={img} alt='upload_img'/>
-          <button type='button' className='custom__button' onClick={HandleClick}><p className='custom__button-text'>Upload</p></button>
+          <button type='button' className='custom__button' onClick={HandleClickImg}><p className='custom__button-text'>Upload</p></button>
         </div>
         <div className='app__admin-upload flex__center'>
           <p className='p__opensans' style={{"font-size":'50px',"padding-bottom":'2rem'}}>Changer les textes</p>
           <div className='app__admin-upload-checkbox flex__center'>
-            <div>
-              <input type="checkbox" id="txtFR" name="txtFR" onChange={handleChange}/>
-              <label for="scales" style={{color:'black','margin':'1rem'}}>FR</label>
-            </div>
-            <div>
-              <input type="checkbox" id="txtEN" name="txtFEN" onChange={handleChange}/>
-              <label for="scales" style={{color:'black','margin':'1rem'}}>EN</label>
-            </div>
-            <div>
-              <input type="checkbox" id="txtDE" name="txtDE" onChange={handleChange}/>
-              <label for="scales" style={{color:'black','margin':'1rem'}}>DE</label>
-            </div>
+          <Box sx={{ minWidth: 80 }}>
+            <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Langues</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={lang}
+                label="langue"
+                onChange={handleChange}>
+                <MenuItem value='fr'>FR</MenuItem>
+                <MenuItem value='en'>EN</MenuItem>
+                <MenuItem value='de'>DE</MenuItem>
+              </Select>
+            </FormControl>
+            </Box>
           </div>
           <input type="file" onChange={getFileInfo}></input>
-          <button type='button' className='custom__button' onClick={HandleClick}><p className='custom__button-text'>Upload</p></button>
+          <button type='button' className='custom__button' onClick={handleClickTxt}><p className='custom__button-text'>Upload</p></button>
         </div>
     </div>
   )
