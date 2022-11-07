1eb5466dd10b02620e521c253a86d34e960a36d5
fresm4at <fernandesvalentin1235@gmail.com>
2022-11-07 18:07:20 +0100

    test

--- server/app.js
+++ server/app.js
@@ -8,7 +8,9 @@ const path = require('path')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const corsOrigin = '*';
 app.use(cors({
+  origin:[corsOrigin],
   methods:['GET','POST'],
   credentials: true 
 })); 
@@ -59,9 +61,7 @@ app.post('/txt-upload-de', txtDEUpload.array('my-image-file'), (req, res, next) 
 })
 app.post('/login',(req,res, next) =>{
   console.log('POST request received to /login');
-  console.log(req.body.user)
-  console.log(req.body.password)
-  if(req.body.user === 'aurelien' && req.body.password==='Jaipas900830Cabane!'){
+  if(req.body.user === 'aurelien' && req.body.password==='test'){
     console.log('auth succes');
     res.send({'auth':'true','token':'McQfTjWnZr4t7w!z%C*F-JaNdRgUkXp2'});
   }else{
