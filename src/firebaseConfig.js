f4417f3a937d0ee02a020f79643c5ea114a7bcdd
fresm4at <fernandesvalentin1235@gmail.com>
2022-12-06 10:31:57 +0100

    remove firebaseConfig

--- src/firebaseConfig.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    apiKey: "AIzaSyCshrqhHugjyyDi9TCHkTFd2v16hTIBZwg",
-    authDomain: "js-website-restaurant.firebaseapp.com",
-    databaseURL: "https://js-website-restaurant-default-rtdb.europe-west1.firebasedatabase.app/"
-  };
-  
-  export default config;
