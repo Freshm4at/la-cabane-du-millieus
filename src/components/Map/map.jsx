821650b4c9160ba97fc24dd1d195cd2397df7900
fresm4at <fernandesvalentin1235@gmail.com>
2022-12-12 11:51:15 +0100

    change google-react-map to ifram embeded map

--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import GoogleMapReact from 'google-map-react';
+//import GoogleMapReact from 'google-map-react';
 import './map.css'
 
 
@@ -19,17 +19,17 @@ export default function Map(){
     });
     return marker;
    };
+   /*      
+   <GoogleMapReact
+    bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+    defaultCenter={defaultProps.center}
+    defaultZoom={defaultProps.zoom}
+    onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}>
+   </GoogleMapReact>
+   */
   return (
-    
-    // Important! Always set the container height explicitly
-    <div classname="google_map" style={{ height: '100vh', width: '100%' }}>
-      
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-        onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}>
-        </GoogleMapReact>
+    <div classname="google_map" style={{ height: '100%', width: '100%' }}>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.6020658382276!2d-1.1290721796778609!3d45.77916466335991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48016503cced1ab5%3A0xabaf18d5be441c43!2sLa%20Cabane%20du%20mil%E2%80%99%20lieus!5e0!3m2!1sfr!2sfr!4v1670841499346!5m2!1sfr!2sfr" style={{border:0, width:"100%",height:"600px" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 }
