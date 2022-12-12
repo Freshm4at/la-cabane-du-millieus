a2c252041cd10e9c2ec00e93bc6872bfe5669673
fresm4at <fernandesvalentin1235@gmail.com>
2022-12-12 16:19:52 +0100

    remove lazy load

--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -30,7 +30,7 @@ export default function Map(){
    */
   return (
     <div classname="google_map" style={{ height: '100%', width: '100%' }}>
-        <iframe title='Google Maps' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.6020658382276!2d-1.1290721796778609!3d45.77916466335991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48016503cced1ab5%3A0xabaf18d5be441c43!2sLa%20Cabane%20du%20mil%E2%80%99%20lieus!5e0!3m2!1sfr!2sfr!4v1670841499346!5m2!1sfr!2sfr" style={{border:0, width:"100%",height:"600px" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe title='Google Maps' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.6020658382276!2d-1.1290721796778609!3d45.77916466335991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48016503cced1ab5%3A0xabaf18d5be441c43!2sLa%20Cabane%20du%20mil%E2%80%99%20lieus!5e0!3m2!1sfr!2sfr!4v1670841499346!5m2!1sfr!2sfr" style={{border:0, width:"100%",height:"600px" }} allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 }
